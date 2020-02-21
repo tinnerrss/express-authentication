ae5fe2398ced5479781db2b4288e8de7adc58654
tinnerrss <tinnerrss@hotmail.com>
2020-02-20 16:47:23 -0800

    yayy only 3 failing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const passport = require('../config/ppConfig');
 
-router.get('/signup', function(req, res) {
+router.get('/signup',(req, res) => {
   res.render('auth/signup');
 });
 
-router.get('/login', function(req, res) {
+router.post('/signup', (req, res) => {
+  db.user.findOrCreate({
+    where: {
+      email: req.body.email
+    }, defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(([user, created]) => {
+    if(created) {
+      console.log("user created");
+      res.redirect('/');
+    } else {
+      console.log("email already exists");
+      res.redirect('/auth/signup');
+    }
+  }).catch(err => {
+    console.log('💩 finding or creating user');
+    console.log(err);
+    res.redirect('/auth/signup')
+  })
+});
+
+router.get('/login',(req, res) => {
   res.render('auth/login');
 });
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login'
+}));
 
 module.exports = router;
